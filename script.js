5f39c0543a002668730bea481f0005f48af3ab87
= <=>
2023-10-24 19:43:53 -0500

    js

---
+++ script.js
@@ -0,0 +1,11 @@
+const signInBtnLink = document.querySelector('.signIn-link');
+const signUpBtnLink = document.querySelector('.signUp-link');
+const wrapper = document.querySelector('.wrapper');
+
+signUpBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+});
+
+signInBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+});
